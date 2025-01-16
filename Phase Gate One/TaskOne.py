a0cecb574b32b79733af13a5a240c443800b87b8
Quayyum-a <quayyumariyo@gmail.com>
2025-01-16 16:02:26 +0100

    phase gate

---
+++ Phase Gate One/TaskOne.py
@@ -0,0 +1,19 @@
+import random
+
+num1 = random.randrange(0, 100)
+num2 = random.randrange(0, 100)
+
+print(num1, " + ", num2)
+
+sum_result = num1 + num2
+
+try: 
+    result = int(input("Enter the sum of both digits: "))
+except ValueError:
+    print("Invalid input, Please retry and enter an integer.")
+    exit()
+else:
+    if result == sum_result:
+        print("True")
+    else:
+        print("False")
