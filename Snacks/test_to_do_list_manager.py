b7e5370e499328ad7297ea9905db9843ca8cd2e9
Quayyum-a <quayyumariyo@gmail.com>
2025-02-25 16:27:22 +0100

    commit

---
+++ Snacks/test_to_do_list_manager.py
@@ -0,0 +1,44 @@
+import unittest
+import todolistmanager
+
+class TestToDoListManager(unittest.TestCase):
+    def test_that_to_do_list_prints_menu(self):
+        actual = todolistmanager.get_menu()
+        expected = """\n1. Add a task
+2. View tasks
+3. Mark task as complete
+4. Delete a task
+5. Exit"""
+        self.assertEqual(actual, expected)
+        
+    def test_that_to_do_list_prints_choice(self):
+        actual = todolistmanager.get_choice()
+        self.assertIsInstance(actual, int)
+        
+    def test_that_to_do_list_add_task(self):
+        actual = todolistmanager.add_task()
+        result = "Buy groceries"
+        self.assertEqual(actual, result)
+        
+    def test_that_to_do_list_view_task(self):
+        todolistmanager.tasks = []  
+        actual = todolistmanager.view_tasks()
+        self.assertEqual(actual, "No task recorded yet.")  
+        
+        todolistmanager.tasks.append({"task": "Buy groceries"})  
+        actual = todolistmanager.view_tasks()
+        expected = "Tasks:\n1 [] Buy groceries" 
+        self.assertEqual(actual, expected)
+        
+    def test_that_to_do_list_mark_complete(self):
+        tasks = [{"task": "Buy groceries", "completed": False}]
+        task_number = 0  
+        tasks[task_number]["completed"] = True 
+
+        expected = True
+        actual = tasks[task_number]["completed"]
+        
+        self.assertTrue(actual, expected)
+    
+if __name__ == "__main__":
+    unittest.main()
---
+++ Snacks/todolistmanager.py
@@ -0,0 +1,49 @@
+tasks = []  
+
+def get_heading():
+    return "To-Do List Manager"
+    
+def get_menu():
+    return """\n1. Add a task
+2. View tasks
+3. Mark task as complete
+4. Delete a task
+5. Exit"""
+
+def get_choice():
+    choice = input("Enter your choice: ")
+    if not choice.isdigit() or int(choice) not in range(1, 6):
+        raise ValueError("Invalid choice. Please enter a number between 1 and 5.")
+    return int(choice)
+    
+def add_task():
+    while True:
+        task = input("Enter a task: ").strip()
+        if task.replace(" ", "").isalpha():  
+            tasks.append({"task": task})  
+            return task
+        else:
+            print("Invalid input! Please enter only letters A-Z.")
+        
+def view_tasks():
+    if not tasks:
+        return "No task recorded yet."
+    else:
+        result = "Tasks:\n"
+        for count, task in enumerate(tasks, start = 1):  
+            status = "[x]" if task.get("completed") else "[]"  
+            result += f"{count} {status} {task['task']}\n"  
+        return result.strip()
+        
+        
+def mark_complete():
+    if not tasks:
+        print("No tasks to mark as complete.")
+        return
+        task_number = int(input("Enter the task number to mark as complete: ")) - 1
+        if 0 <= task_number < len(tasks):
+            tasks[task_number]["completed"] = True  
+            print(f"Task '{tasks[task_number]['task']}' completed.")
+        else:
+            print("Invalid task number. Please enter a valid number.")
+    
