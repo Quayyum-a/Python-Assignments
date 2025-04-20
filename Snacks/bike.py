15e88aee941c258f11d5bb8235ab96bc7b9cb71b
Quayyum-a <quayyumariyo@gmail.com>
2025-04-20 18:56:32 +0100

    commited

---
+++ Snacks/bike.py
@@ -0,0 +1,15 @@
+class My_Bike:
+    def __init__(self):
+        self.IsOn = False
+
+    def setOn(self):
+        return self.IsOn
+
+    def TurnOn(self):
+        self.IsOn = True
+
+    def TurnOff(self):
+        self.IsOn = False
+
+    def Accelerate(self):
+        pass
